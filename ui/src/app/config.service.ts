6c85ed83055c1774b4456f33424f4a2765d6ec6b
Patrick Pacher <patrick.pacher@gmail.com>
2024-02-13 17:40:27 +0100

    ui: add definitions for extra user fields

--- ui/src/app/config.service.ts
+++ ui/src/app/config.service.ts
@@ -1,5 +1,22 @@
 import { Injectable } from '@angular/core';
 
+export interface PossibleValue {
+  value: string;
+  display_name: string;
+}
+
+export interface FieldConfig {
+  type: 'string' | 'number' | 'bool' | 'object' | 'list' | 'any' | 'date' | 'time';
+  name: string;
+  visibility: 'public' | 'self' | 'private' | 'authenticated';
+  writeable: boolean | null;
+  description: string;
+  display_name: string;
+  property: FieldConfig[] | null;
+  element_type: FieldConfig | null;
+  possible_value: PossibleValue[] | null;
+}
+
 export interface RemoteConfig {
   domain: string;
   loginURL: string;
@@ -10,6 +27,7 @@ export interface RemoteConfig {
   userAddresses: boolean;
   phoneNumbers: boolean;
   userNameChange: boolean;
+  customUserFields: FieldConfig[] | null;
 }
 
 @Injectable({ providedIn: 'root' })
